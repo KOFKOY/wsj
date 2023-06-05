14a53ec853070b2b4783be45e26c59b671df790f
王帅杰 <shuaijie.wang@ziel.cn>
2023-06-05 16:35:40 +0800

    init

--- public.sh
+++ public.sh
@@ -1,12 +1,13 @@
 mvn clean & mvn compile & mvn package -Dmaven.test.skip=true
 echo "打包成功"
-cd server
-cd target
 # 获取与 java -jar 相关的进程ID列表
 pids=$(ps -aux | grep "java -jar" | grep -v grep | awk '{print $2}')
 # 关闭进程
 for pid in $pids; do
     kill -9 $pid
 done
-nohup java -jar server-1.0.jar > log.txt &
+echo "关闭进程"
+cd server
+cd target
+nohup java -jar server-1.0.jar &
 echo "发布成功"
