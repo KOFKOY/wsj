1fec477131f698390da0ad17ad3526121467668c
王帅杰 <shuaijie.wang@ziel.cn>
2023-06-05 16:36:47 +0800

    init

--- public.sh
+++ public.sh
@@ -3,7 +3,9 @@ echo "打包成功"
 # 获取与 java -jar 相关的进程ID列表
 pids=$(ps -aux | grep "java -jar" | grep -v grep | awk '{print $2}')
 # 关闭进程
+echo "正在运行的进程ID"
 for pid in $pids; do
+    echo $pid
     kill -9 $pid
 done
 echo "关闭进程"
