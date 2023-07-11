584b616f6dd0edda196f04f59b7428a3d75f7795
王帅杰 <shuaijie.wang@ziel.cn>
2023-07-11 14:02:14 +0800

    fs

--- server/src/main/java/com/wsj/server/controller/FeishuController.java
+++ server/src/main/java/com/wsj/server/controller/FeishuController.java
@@ -1,16 +1,21 @@
 package com.wsj.server.controller;
 
+import cn.hutool.json.JSONUtil;
+import com.wsj.server.util.NoticeUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/fs")
 public class FeishuController {
+    @Resource
+    private NoticeUtil noticeUtil;
     @PostMapping
     public Map feishu(@RequestBody Map map) {
         Map result = new HashMap();
@@ -18,6 +23,7 @@ public class FeishuController {
             result.put("challenge", map.get("challenge"));
             return result;
         }
+        noticeUtil.send(JSONUtil.toJsonStr(map));
         return result;
     }
 }
